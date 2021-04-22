9a80e350dd31221bce4d8870957b47b772d0fde1
Muhammad Usman <80705139+usmanjd25@users.noreply.github.com>
2021-04-22 01:32:04 -0700

    Add files via upload

---
+++ 03_pr_03_module_usage.py
@@ -0,0 +1,4 @@
+'''from playsound import playsound
+playsound('D:\\MyData\\Business\\code playground\\Python Course With Notes\\1. Chapter 1\\play.mp3')'''
+from playsound import playsound
+playsound('E:\\Python Course with Notes\\1. Chapter 1\\play.mp3')
