7290809608c4f19fea2e0fb60ace63fa6571d23c
Muhammad Usman <80705139+usmanjd25@users.noreply.github.com>
2021-04-22 01:33:30 -0700

    Update 02_pr_01_twinkle.py

--- 02_pr_01_twinkle.py
+++ 02_pr_01_twinkle.py
@@ -1,9 +1,3 @@
-'''
-This problem is a solution of
-Problem 1 of CodeWithHarry Practice Set!
-'''
-# This is also a comment just like the above line
-
 print('''Twinkle, twinkle, little star,
 How I wonder what you are!
 Up above the world so high,
